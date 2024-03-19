bf6b5e7e60d07e4550389e5eb2ea2ef0893c2048
Dawid Domiński <dawid.dominski1125@gmail.com>
2024-03-19 18:24:22 +0100

    feat: rename and move uslugi route to services dir

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import ContactPage from './components/Routes/ContactPage';
 import HomePage from './components/Routes/HomePage';
 import About from './components/Routes/About';
-import Uslugi from './components/Routes/Uslugi';
+import Services from './components/Routes/services/services';
 import TermsOfUse from './components/Routes/TermsOfUse';
 
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
 
@@ -13,7 +13,7 @@ function App() {
       <Routes>
         <Route exact path = "/Harvium-LandingPage/" element={<HomePage />}></Route>
         <Route exact path = "/Harvium-LandingPage/Handluj" element={<ContactPage />}></Route>
-        <Route exact path = "/Harvium-LandingPage/Uslugi" element={<Uslugi />}></Route>
+        <Route exact path = "/Harvium-LandingPage/Uslugi" element={<Services />}></Route>
         <Route exact path = "/Harvium-LandingPage/About" element={<About />}></Route>
         <Route exact path = "/Harvium-LandingPage/Contact" element={<ContactPage />}></Route>
         <Route exact path = "/Harvium-LandingPage/TermsOfUse" element={<TermsOfUse />}></Route>
---
+++ src/components/Routes/services/services.jsx
@@ -0,0 +1,12 @@
+import NavBar from '../../custom/NavBar.jsx'
+import Footer from '../../custom/Footer.jsx'
+
+const Services = () => {
+  return (
+    <>
+      <NavBar />
+      <Footer />
+    </>
+  )
+}
+export default Services
