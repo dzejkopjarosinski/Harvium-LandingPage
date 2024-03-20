abcd09d9750ba77cdfb49f4f70bcd12519986582
Dawid Domiński <dawid.dominski1125@gmail.com>
2024-03-20 12:44:41 +0100

    feat: use Reveal animation on Service content and ourServices header

--- src/components/Routes/services/ourServices/ourServices.jsx
+++ src/components/Routes/services/ourServices/ourServices.jsx
@@ -1,29 +1,35 @@
 import Service from "@/components/Routes/services/ourServices/service/service.jsx";
 import serviceIcon from "@/assets/services/serviceDefaultIcon.png"
+import Reveal from "@/components/animations/reveal/reveal.jsx";
+
 const OurServices = () => {
 
     return (
-        <div className='bg-background-primary flex flex-col justify-center items-center gap-16 font-roboto text-white font-bold'>
-            <h1 className='text-[40px] mt-12'>Nasze Usługi</h1>
-            <div className='flex flex-wrap justify-center mb-32 p-5 gap-28'>
-                <Service
-                    logo={serviceIcon}
-                    serviceHeading={'Long heading is what you see here in this feature section'}
-                    serviceDescription={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla.'}
-                    serviceRoute={'/Harvium-LandingPage/'}
-                />
-                <Service
-                    logo={serviceIcon}
-                    serviceHeading={'Long heading is what you see here in this feature section'}
-                    serviceDescription={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla.'}
-                    serviceRoute={'/Harvium-LandingPage/'}
-                />
-                <Service
-                    logo={serviceIcon}
-                    serviceHeading={'Long heading is what you see here in this feature section'}
-                    serviceDescription={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla.'}
-                    serviceRoute={'/Harvium-LandingPage/'}
-                />
+        <div className='bg-background-primary font-roboto text-white font-bold'>
+            <div className='flex flex-col justify-center items-center gap-16 px-5 py-14'>
+                <Reveal>
+                    <h1 className='text-[40px]'>Nasze Usługi</h1>
+                </Reveal>
+                <div className='flex flex-wrap justify-center mb-32 gap-28'>
+                    <Service
+                        logo={serviceIcon}
+                        serviceHeading={'Long heading is what you see here in this feature section'}
+                        serviceDescription={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla.'}
+                        serviceRoute={'/Harvium-LandingPage/'}
+                    />
+                    <Service
+                        logo={serviceIcon}
+                        serviceHeading={'Long heading is what you see here in this feature section'}
+                        serviceDescription={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla.'}
+                        serviceRoute={'/Harvium-LandingPage/'}
+                    />
+                    <Service
+                        logo={serviceIcon}
+                        serviceHeading={'Long heading is what you see here in this feature section'}
+                        serviceDescription={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla.'}
+                        serviceRoute={'/Harvium-LandingPage/'}
+                    />
+                </div>
             </div>
         </div>
     );
--- src/components/Routes/services/ourServices/service/service.jsx
+++ src/components/Routes/services/ourServices/service/service.jsx
@@ -1,16 +1,25 @@
 import { Link } from "react-router-dom";
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import Reveal from "@/components/animations/reveal/reveal.jsx";
+
 
 const Service = ({ logo, serviceHeading, serviceDescription, serviceRoute }) => {
+
     return (
         <div className='flex flex-col justify-center items-center max-w-[405px] text-center font-normal gap-6'>
             <img src={logo} className='size-12' alt='service logo'/>
-            <div className='text-2xl font-bold'>{serviceHeading}</div>
-            <p>{serviceDescription}</p>
-            <Link to={serviceRoute} className='flex justify-center items-center gap-1'>
-                <div>Sprawdź ofertę</div>
-                <NavigateNextIcon />
-            </Link>
+            <Reveal>
+                <div className='text-2xl font-bold'>{serviceHeading}</div>
+            </Reveal>
+            <Reveal>
+                <p>{serviceDescription}</p>
+            </Reveal>
+            <Reveal>
+                <Link to={serviceRoute} className='flex justify-center items-center gap-1'>
+                    <div>Sprawdź ofertę</div>
+                    <NavigateNextIcon />
+                </Link>
+            </Reveal>
         </div>
     );
 };
