d3cec6c8ad91e9a1084e8aa9afc993ff4af5eb5f
Dawid Domiński <dawid.dominski1125@gmail.com>
2024-03-21 01:38:08 +0100

    feat: update ServiceCard design, adjust RWD, add assets serviceCardPhoto

--- src/components/Routes/services/servicesDescription/servicesDescription.jsx
+++ src/components/Routes/services/servicesDescription/servicesDescription.jsx
@@ -1,5 +1,6 @@
 import Reveal from "@/components/animations/reveal/reveal.jsx";
 import ServiceCard from "@/components/Routes/services/servicesDescription/serviceCard/serviceCard.jsx";
+import serviceCardPhoto from "@/assets/services/serviceDescriptionImage.png"
 
 
 const ServicesDescription = () => {
@@ -13,9 +14,27 @@ const ServicesDescription = () => {
                 <Reveal>
                     <span className='text-lg'>lorem ipsum</span>
                 </Reveal>
-                <div className='flex flex-wrap justify-center gap-48 pt-24'>
-                    <ServiceCard />
-                    <ServiceCard />
+                <div className='flex flex-wrap justify-center gap-48 pt-24 px-5'>
+                    <ServiceCard
+                        serviceHeader={'Lorem ipsum dolor sit amet ipsum dolor lorem'}
+                        serviceDescription={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc odio in et, lectus sit lorem id integer.'}
+                        servicePhoto={serviceCardPhoto}
+                    />
+                    <ServiceCard
+                        serviceHeader={'Lorem ipsum dolor'}
+                        serviceDescription={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc odio in et, lectus sit lorem id integer. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc odio in et, lectus sit lorem id integer. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc odio in et, lectus sit lorem id integer.'}
+                        servicePhoto={serviceCardPhoto}
+                    />
+                    <ServiceCard
+                        serviceHeader={'Lorem ipsum dolor'}
+                        serviceDescription={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc odio in et, lectus sit lorem id integer.'}
+                        servicePhoto={serviceCardPhoto}
+                    />
+                    <ServiceCard
+                        serviceHeader={'Lorem ipsum dolor'}
+                        serviceDescription={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc odio in et, lectus sit lorem id integer.'}
+                        servicePhoto={serviceCardPhoto}
+                    />
                 </div>
             </div>
         </div>
